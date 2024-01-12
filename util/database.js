4bf7cbce783c498a9ead2b32e818e5dbd212cb65
andres sanchez <andres.sanchez.perez@udla.edu.ec>
2024-01-11 19:45:25 -0500

    Update Database

--- util/database.js
+++ util/database.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
     host:config.host,
     user:config.user,
     database:config.database,
-    password:config.password
+    password:config.password,
+    port:config.port
 })
 
 
