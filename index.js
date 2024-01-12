33b4899b2bca7924ad824fafc2be296100b48843
andres sanchez <andres.sanchez.perez@udla.edu.ec>
2024-01-12 00:18:54 -0500

    update

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
-
 const authRoutes = require('./routes/auth');
-
-const apiRoutes = require('./routes/api')
-
-const errorController = require('./Controllers/errorController')
+const apiRoutes = require('./routes/api');
+const errorController = require('./Controllers/errorController');
 
 const app = express();
-
-const ports = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-    next();
-});
-
-app.use('/auth',authRoutes);
-
-app.use('/api',apiRoutes);
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false, // Enable credentials (e.g., cookies, authorization headers)
-    optionsSuccessStatus: 204, // Respond with a 204 status for preflight requests
-  };
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: false,
+  optionsSuccessStatus: 204,
+};
 
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.sendStatus(204);
-  });
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.sendStatus(204);
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(errorController.get404);
 app.use(errorController.get500);
 
-
-app.listen(ports, ()=>console.log(`listening on ${ports}`));
+app.listen(port, () => console.log(`Listening on ${port}`));
