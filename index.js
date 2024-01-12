08f054828e39cfa1b7125233515ffa4052b0ce34
andres sanchez <andres.sanchez.perez@udla.edu.ec>
2024-01-12 00:40:26 -0500

    update index,js

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); // Import the cors module
+
 const authRoutes = require('./routes/auth');
 const apiRoutes = require('./routes/api');
 const errorController = require('./Controllers/errorController');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const ports = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: false,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.sendStatus(204);
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(errorController.get404);
 app.use(errorController.get500);
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(ports, () => console.log(`listening on ${ports}`));
